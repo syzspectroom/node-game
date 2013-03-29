d004bd7f2331d6c1d0398beef94b07521a7f54ff
blacky <syzspectroom@gmail.com>
2013-03-29 18:06:57 +0000

    backend

--- events.js
+++ events.js
@@ -1,5 +1,7 @@
 var io = require('socket.io');
-var clients = [];
+var backend = require('./backend');
+
+var socketClients = [];
 exports.setServer = function(server) {
 	io = io.listen(server);
 }
@@ -9,26 +11,35 @@ exports.init = function() {
   io.sockets.on('connection', function (socket) {
   	  
   	initNewPlayer(socket);
+  	initPlayerMoves(socket);
 
   	socket.on('disconnect', playerDisconnected );
-
   });
 
+  function initPlayerMoves(socket) {
+	socket.on('engineOn', backend.engineOn(socket.id) );
+	socket.on('engineOff', backend.engineOff(socket.id)  );
+	socket.on('incAngleVelocity', backend.incAngleVelocity(socket.id)  );
+	socket.on('decAngleVelocity', backend.decAngleVelocity(socket.id)  );  	
+  }
+
   function playerDisconnected() {
-  	delete clients[this.id];
+  	delete socketClients[this.id];
+  	backend.removePlayer(this.id);
   	console.log('player disconected');
-	for (id in clients) {
-	   	clients[id].emit('player_disconnected', { id: this.id });
+	for (id in socketClients) {
+	   	socketClients[id].emit('player_disconnected', { id: this.id });
   	}
   }
 
   function initNewPlayer(socket) {
   	socket.emit('init_player', { id: socket.id });
   	  console.log('new player');
-  	  for (id in clients) {
-  	  	clients[id].emit('new_player_registered', { id: socket.id });
+  	  for (id in socketClients) {
+  	  	socketClients[id].emit('new_player_registered', { id: socket.id });
   	  }		
-  	  clients[socket.id] = socket;
+  	  socketClients[socket.id] = socket;
+  	  backend.addPlayer(socket.id);
   }
 
 }
